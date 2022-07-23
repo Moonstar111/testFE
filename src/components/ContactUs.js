08d8c7be35f0ffee3628fc8650fe2101401241aa
Sony <sohni2012@gmail.com>
2022-07-23 19:18:47 +0530

    wallet connect

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -38,7 +38,7 @@ export default function Contact() {
                   <div class="error-message"></div>
                   <div class="sent-message">Your message has been sent. Thank you!</div>
                 </div>
-                <div class="text-center"><button type="submit">Send Message</button></div>
+                <div class="text-center"><button  className="neon_btn" type="submit">Send Message</button></div>
               </form>
             </div>
   
